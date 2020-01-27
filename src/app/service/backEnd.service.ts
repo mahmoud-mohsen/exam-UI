08668bbc50d5925a24882d5789f75620f7c019f0
Mahmoud Mohsen <mahmoudmohsen335@gmail.com>
2020-01-27 02:06:23 +0200

    connect with heroku app

--- src/app/service/backEnd.service.ts
+++ src/app/service/backEnd.service.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/catch'; // don't forget this, or you'll get a runtime 
 })
 export class GlobalBackEndService {
 
-  baseUrl: string = 'https://junko-ui.herokuapp.com/';
+  baseUrl: string = 'https://junko.herokuapp.com/';
 
   activeUser
   httpHeaders = new HttpHeaders({
