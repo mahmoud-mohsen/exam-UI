7fc943681110f70a2bb29af1c3255358d83a8628
Mahmoud Mohsen <mahmoudmohsen335@gmail.com>
2020-02-15 19:44:42 +0200

    Translate login page

--- src/app/component/layout/layout.component.ts
+++ src/app/component/layout/layout.component.ts
@@ -29,7 +29,7 @@ export class LayoutComponent implements OnInit {
       this.language = localStorage.getItem('locale');
 
     } else {
-      localStorage.setItem('locale', 'en');
+      localStorage.setItem('locale', 'ar');
     }
 
   }
